99e1a7a074c9cf7ab008a5087956c41fdb286f0b
Nagabhushanam <nagabhushanamn@gmail.com>
2018-04-11 13:16:46 +0530

    arrow function

--- JS-level-2/1-new-syntax.js
+++ JS-level-2/1-new-syntax.js
@@ -1,3 +1,4 @@
+"use strict"
 
 //-----------------------------------------------------
 // 4. object-lieral-enhancements
@@ -81,16 +82,16 @@ function func(a, b, c) {
     console.log(c);
 }
 
-let nums = [10, 20, 30];
+// let nums = [10, 20, 30];
 // func(nums[0], nums[1], nums[2]);
 // or
 // func(...nums);
 
 
-let a1 = [1, 2, 3]
-let a2 = [7, 8, 9]
+// let a1 = [1, 2, 3]
+// let a2 = [7, 8, 9]
 
-let newArr = [...a1, 4, 5, 6, ...a2];
+// let newArr = [...a1, 4, 5, 6, ...a2];
 
 
 //-----------------------------------------------------
@@ -154,7 +155,7 @@ let newArr = [...a1, 4, 5, 6, ...a2];
 // }
 
 //-----------------------------------------------------------
-// 9. OO with class-syntax
+// OO with class-syntax
 //-----------------------------------------------------------
 
 
@@ -173,49 +174,194 @@ let newArr = [...a1, 4, 5, 6, ...a2];
 
 // Es6
 
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-        console.log('Person::constructor()');
-    }
-    sayName() {
-        console.log('im ' + this.name);
-    }
-    sayAge() {
-        console.log('im ' + this.age + "old");
-    }
-}
-// var p1 = new Person('Nag', 33);
+// class Person {
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//         console.log('Person::constructor()');
+//     }
+//     sayName() {
+//         console.log('im ' + this.name);
+//     }
+//     sayAge() {
+//         console.log('im ' + this.age + "old");
+//     }
+// }
+// // var p1 = new Person('Nag', 33);
 
-class Employee extends Person {
-    constructor(name, age, salary) {
-        super(name, age);
-        this.salary = salary;
-        console.log('Employee::construtor');
-    }
-    saySalary() {
-        console.log('im ' + this.salary + " only");
-    }
-    askForRaise() {
-        return this.salary * 0.02;
-    }
-}
+// class Employee extends Person {
+//     constructor(name, age, salary) {
+//         super(name, age);
+//         this.salary = salary;
+//         console.log('Employee::construtor');
+//     }
+//     saySalary() {
+//         console.log('im ' + this.salary + " only");
+//     }
+//     askForRaise() {
+//         return this.salary * 0.02;
+//     }
+// }
 
-// let e = new Employee('Nag', 33, 1000.00);
+// // let e = new Employee('Nag', 33, 1000.00);
 
-class Boss extends Employee {
-    askForRaise() {
-        return this.salary * 0.2 + super.askForRaise();
+// class Boss extends Employee {
+//     askForRaise() {
+//         return this.salary * 0.2 + super.askForRaise();
+//     }
+// }
+
+// let boss = new Boss('Nag', 33, 1000.00);
+
+
+// class Abc {
+//     static staMethod() {
+//         console.log('....');
+//     }
+// }
+// Abc.staVar = 123; // static variable
+
+
+
+//-----------------------------------------------------------
+// arrow function
+//-----------------------------------------------------------
+
+
+// function-expression
+
+// let getPrice = function () {
+//     return 100 + 200;
+// }
+
+// arrow-function
+
+// let getPrice = () => {
+//     return 100 + 200;
+// }
+
+// let getPrice = (count) => {
+//     return count * (100 + 200);
+// }
+// or
+// let getPrice = count => {
+//     return count * (100 + 200);
+// }
+
+
+// let getPrice = (count, tax) => {
+//     return count * (100 + 200) + tax;
+// }
+// or
+// let getPrice = (count, tax) => count * (100 + 200) + tax;
+
+// let getPrice = (count, tax) => {
+//     let cost = count * (100 + 200);
+//     let total = cost + tax;
+//     return total;
+// }
+
+
+/*
+
+    where/why we need arrow-function?
+
+    ==> for compact function-argument
+    ==> to capture 'this'  ( most important )
+
+*/
+
+// ==> for compact function-argument
+
+// e.g
+
+// let nums = [1, 3, 5, 7, 9, 2, 4, 6, 8, 10];
+// // nums.sort();
+// // nums.sort(function (a, b) { return a - b; });
+// // or
+// nums.sort((a, b) => a - b);
+// console.log(nums);
+
+
+
+// ==> to capture 'this'  ( most important )
+
+
+/*
+
+ in javascript,
+ by default any function statically or dynamically 
+ we can bind to other object(s)
+
+*/
+
+// let tnr = {
+//     name: 'Nag',
+//     doTeach() {
+//         console.log(this.name + " teaching .js");
+//         // let askQues = function (q) {
+//         //     console.log(this.name + " anwering " + q);
+//         // }
+//         // or
+//         let askQues = (q) => {
+//             console.log(this.name + " anwering " + q);
+//         }
+//         console.log('teaching end...');
+//         return askQues;
+//     }
+// }
+
+// let askQues = tnr.doTeach();
+// askQues("Q1");
+// let newTnr = { name: 'Praveen' }
+// askQues.call(newTnr, "Q2")
+
+//-----------------------------------------------------------------
+
+
+// let invoice = {
+//     num: 123,
+//     process: function () {
+//         console.log('INV-' + this.num + ' processed..');
+//     }
+// };
+// invoice.process();
+
+// let invoice = {
+//     num: 123,
+//     process: () => {
+//         console.dir(this);
+//         console.log('INV-' + this.num + ' processed..');
+//     }
+// };
+// invoice.process();
+
+// let invoice = {
+//     num: 123,
+//     process: function () {
+//         console.log('INV-' + this.num + ' processed partially');
+//         let complete = function () {
+//             console.log('INV-' + this.num + ' processed completly');
+//         }
+//         return complete;
+//     }
+// };
+
+// let complete=invoice.process();
+// complete(); 
+
+
+
+let invoice = {
+    num: 123,
+    process: function () {
+        console.log('INV-' + this.num + ' processed partially');
+        let complete = () => {
+            console.log('INV-' + this.num + ' processed completly');
+        }
+        return complete;
     }
-}
+};
 
-let boss = new Boss('Nag', 33, 1000.00);
-
-
-class Abc {
-    static staMethod() {
-        console.log('....');
-    }
-}
-Abc.staVar = 123;
+let complete = invoice.process();
+complete(); 
