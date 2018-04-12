cf9eac4be7b6f0bb9d0f53bbc250ee840b309711
Nagabhushanam <nagabhushanamn@gmail.com>
2018-04-12 10:15:59 +0530

    todo app

--- JS-level-2/todo-app.js
+++ JS-level-2/todo-app.js
@@ -4,11 +4,13 @@
 // Model
 class Todo {
     constructor(title) {
-        this.id = 0;
+        Todo.id++;
+        this.id = Todo.id;
         this.title = title;
         this.completed = false;
     }
 }
+Todo.id = 0;
 //-----------------------------------------------------
 // Service
 class TodoService {
@@ -16,25 +18,44 @@ class TodoService {
         this.todos = []
     }
     addTodo(title) {
-        //
+        let newTodo = new Todo(title);
+        this.todos = this.todos.concat(newTodo);
     }
     completeTodo(id) {
-        //
+        this.todos = this.todos.map(todo => Object.assign({}, todo, { completed: todo.id === id ? !todo.completed : false }))
     }
     editTodo(id, newTitle) {
-        //
+        this.todos = this.todos.map(todo => Object.assign({}, todo, { title: todo.id === id ? newTitle : todo.title }))
     }
     deleteTodo(id) {
-
+        this.todos = this.todos.filter(todo => todo.id !== id ? true : false)
     }
     completeAll() {
-
+        let isAllCompleted = this.todos.every(todo => todo.completed);
+        this.todos = this.todos.map(todo => Object.assign({}, todo, { completed: !isAllCompleted }))
     }
     clearCompleted() {
-
+        this.todos = this.todos.filter(todo => !todo.completed)
     }
-    view(status) {
+    view(filter) {
         // status ==> 'ALL' | 'ACTIVE' | 'COMPLETED'
+        switch (filter) {
+            case 'ALL':
+                this.todos.forEach(item => console.log(item))
+                break;
+            case 'ACTIVE':
+                this.todos.forEach(item => {
+                    if (!todo.completed) console.log(todo)
+                })
+                break;
+            case 'COMPLETED':
+                this.todos.forEach(item => {
+                    if (todo.completed) console.log(todo)
+                })
+                break;
+            default:
+                console.log(' give me valid filter');
+        }
     }
 
 }
