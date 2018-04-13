b21f4b6f81c5a063343d4446527a933b401cfb06
Nagabhushanam <nagabhushanamn@gmail.com>
2018-04-13 15:07:39 +0530

    es modules

--- 4-JS-modules/index.js
+++ 4-JS-modules/index.js
@@ -14,18 +14,18 @@ console.log('-index.js-');
 
 //---------------------------------------------------------
 
-let ibmGreet = require('ibm-apr-greet');
-let _ = require('lodash');
+// let ibmGreet = require('ibm-apr-greet');
+// let _ = require('lodash');
 
-ibmGreet.greet('en');
-ibmGreet.greet('tn');
-ibmGreet.greet('ka');
-ibmGreet.greet();
-//----------------------------------------------------------
-let a1 = [1, 2, 3]
-let a2 = [3, 4, 5]
-let diff = _.difference(a1, a2);
-console.log(diff);
+// ibmGreet.greet('en');
+// ibmGreet.greet('tn');
+// ibmGreet.greet('ka');
+// ibmGreet.greet();
+// //----------------------------------------------------------
+// let a1 = [1, 2, 3]
+// let a2 = [3, 4, 5]
+// let diff = _.difference(a1, a2);
+// console.log(diff);
 //----------------------------------------------------------
 
 
