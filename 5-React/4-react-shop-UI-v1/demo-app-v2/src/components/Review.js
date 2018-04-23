613f58b8bcc366240e44a152b920581463d4006d
Nagabhushanam <nagabhushanamn@gmail.com>
2018-04-23 14:56:39 +0530

    stars

--- 5-React/4-react-shop-UI-v1/demo-app-v2/src/components/Review.js
+++ 5-React/4-react-shop-UI-v1/demo-app-v2/src/components/Review.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Review extends Component {
+    renderStars(n) {
+        let stars = [];
+        for (let i = 0; i < n; i++) {
+            stars.push(<i className="fa fa-star"></i>);
+        }
+        return stars;
+    }
     render() {
         let { review } = this.props;
         return (
             <div className="row">
                 <div className="col-6 col-sm-8 col-md-8">
                     <div className="alert alert-info">
-                        <span>{review.stars}</span> &mdash; {review.author}
+                        <span>{this.renderStars(review.stars)}</span> &mdash; {review.author}
                         <hr />
                         <p>{review.body}</p>
                     </div>
